c1ec782e54f519fa0a30f49cedf9c541e9414823
east20app <whgamersc@gmail.com>
2026-09-06 22:07:12 -0300

    yrese
    sreere

--- site/app/robots.ts
+++ site/app/robots.ts
@@ -2,5 +2,5 @@ import type { MetadataRoute } from "next";
 import { siteUrl } from "@/lib/site-url";
 
 export default function robots(): MetadataRoute.Robots {
-    return { rules: { userAgent: "*", allow: "/", disallow: ["/dashboard", "/admin", "/api"] }, sitemap: new URL("/sitemap.xml", siteUrl).toString(), host: siteUrl.origin };
+    return { rules: { userAgent: "*", allow: "/", disallow: ["/dashboard", "/dashboard/app/", "/planos"] }, sitemap: new URL("/sitemap.xml", siteUrl).toString(), host: siteUrl.origin };
 }
