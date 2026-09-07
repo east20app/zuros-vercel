2b0130c66d14202839b318acfbf6dc9c2574aa30
east20app <whgamersc@gmail.com>
2026-09-06 23:15:05 -0300

    Update page.tsx

--- site/app/login/page.tsx
+++ site/app/login/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
-import { LoginForm } from "@/components/LoginForm";
+import LoginPage from "@/components/LoginPage";
 
-export default function LoginPage() {
-    return (
-        <Suspense fallback={null}>
-            <LoginForm />
-        </Suspense>
-    );
+export default function Page() {
+    return <LoginPage />;
 }
